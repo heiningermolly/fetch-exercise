a51ba84d0e548d9ae668ac1b2b24df13d782dd69
Molly Heininger <heininger.molly@gmail.com>
2023-10-02 10:06:51 -0500

    completed

--- spring/livecodeapp 2/src/main/java/org/launchcode/livecodeapp/HelloController.java
+++ spring/livecodeapp 2/src/main/java/org/launchcode/livecodeapp/HelloController.java
@@ -1,2 +1,36 @@
-package org.launchcode.livecodeapp;public class HelloController {
+package org.launchcode.livecodeapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello class!";
+    }
+
+    @GetMapping("/hello") //this will recognize it with or without the slash being added, but Mike (and many of us) like it with the slash better!
+    public String getNameForm() {
+        return "<form method='post' action='/hello/hello'>\n" +
+                "  <label>What's your name?\n" +
+                "    <input type='text' name='name' />\n" + //MAKE SURE THE VARIABLE IS THE SAME BETWEEN GET AND POST METHODS
+                "  </label>\n" +
+                "            <input type='submit' value='Submit' />\n" +
+                "            </form>";
+    }
+
+    @PostMapping("/hello")
+    public String handleForm(@RequestParam String name){ // MAKE SURE VARIABLE MATCHES THE GET MAPPING
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/{name}")
+    public String pathTest(@PathVariable String name){
+        return "Sup, " + name + "!";
+    }
+
 }
